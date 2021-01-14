289b1bd56bffbb9fb70fde3e7ed9d584ec169a27
ytyaru <yttry0@gmail.com>
2021-01-14 13:09:50 +0900

    Created a repository

---
+++ docs/1/CacheTest.js
@@ -0,0 +1,40 @@
+class CacheTest {
+    #VERSION = '0.0.1';
+    #CACHE_FILES = [
+        'index.html', 
+        'main.js', 
+        'CacheTest.js', 
+    ];
+    constructor() {
+        console.log(window.Cache);
+        this.open();
+        this.keys();
+//        this.delete();
+    }
+    open() {
+        const self = this;
+        caches.open(this.#VERSION).then(function(cache) {
+            console.log('---- open()', self.#CACHE_FILES);
+            return cache.addAll(self.#CACHE_FILES);
+        });
+    }
+    keys() {
+        caches.keys().then(function(keyList) {
+            console.log('---- keys()');
+            return Promise.all(keyList.map(function(key) {
+                console.log(`  ${key}`);
+                /*
+                if (this.#VERSION != key) {
+                    return caches.delete(key);
+                }
+                */
+            }));
+        });
+    }
+    delete() {
+        const self = this;
+        caches.delete(this.#VERSION).then(function(is_found) {
+            console.log(`---- delete() ${self.#VERSION} is_found: ${is_found}`);
+        });
+    }
+}
